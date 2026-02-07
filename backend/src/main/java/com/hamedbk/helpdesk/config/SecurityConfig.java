c1309fb05718cd78cc2da0daa4febde713e46de2
Hamed <hamedbk@getgoogleoff.me>
2026-02-07 23:53:31 +0100

    Update cors configuration to allow local angular dev frontend only

--- backend/src/main/java/com/hamedbk/helpdesk/config/SecurityConfig.java
+++ backend/src/main/java/com/hamedbk/helpdesk/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
